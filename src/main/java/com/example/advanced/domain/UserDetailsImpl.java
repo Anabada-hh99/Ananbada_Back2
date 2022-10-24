65148f255e6be4ff187ee72c68b0ded4e89359b9
first_commit <>
2022-10-24 09:57:29 +0900

    "2nd commit - change loadByUsername"

--- src/main/java/com/example/advanced/domain/UserDetailsImpl.java
+++ src/main/java/com/example/advanced/domain/UserDetailsImpl.java
@@ -32,7 +32,7 @@ public class UserDetailsImpl implements UserDetails {
 
   @Override
   public String getUsername() {
-    return member.getNickname();
+    return member.getLoginName();
   }
 
   @Override
